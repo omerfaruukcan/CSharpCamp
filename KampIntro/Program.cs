536828443f5173d5bf3467d9de10ba288e8ac5cb
omerf <omerf@OMERFARUKCAN>
2022-03-16 00:24:09 +0300

    Add project files.

---
+++ KampIntro/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // type safety
+            // Do not repeat yourself - DRY
+            // alias
+
+            string kategoriEtiketi = "Kategori";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = true;
+            double dolarDun = 15.35;
+            double dolarBugun = 15.45;
+
+            if (dolarDun > dolarBugun)
+            {
+                Console.WriteLine("Azalis Oku");
+            }
+            else if (dolarDun < dolarBugun)
+            {
+                Console.WriteLine("Artis Oku");
+            }
+            else
+            {
+                Console.WriteLine("Esittir Oku");
+            }
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanici ayarlari butonu burasi");
+            }
+            else
+            {
+                Console.WriteLine("Lutfen Giris Yapiniz butonu");
+            }
+        }
+    }
+}
