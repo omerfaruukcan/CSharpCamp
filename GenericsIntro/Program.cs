3bf5378b77b7ff7f2f0c0bb4b77b2ff928ba03b1
omerfaruukcan <ofarukcan@trakya.edu.tr>
2022-04-21 19:59:46 +0300

    21/04/2022

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Omer Faruk");
+            Console.WriteLine(isimler.Length);
+
+            isimler.Add("Busra");
+            Console.WriteLine(isimler.Length);
+
+            foreach (var isim in isimler.Items)
+            {
+                Console.WriteLine(isim);
+            }
+            
+        }
+    }
+}
